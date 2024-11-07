e395c31c963f64e1975b72e3dc836c30c6fb4344
nercode <nerka020@gmail.com>
2024-11-07 06:03:29 +0200

    Update delete-disk.sh

--- delete-disk.sh
+++ delete-disk.sh
@@ -1,4 +1,6 @@
-# echo -e "./delete-disk.sh <UUID> <disk_name>"
+#!/bin/bash
+# To run this script on Windows you need Msys2 https://www.msys2.org/
+#
 cd /c/Program\ Files/Oracle/VirtualBox
 ./vboxmanage list hdds | tr -s '\\' '\n' | grep -e 'UUID' -e 'Capacity' -e '\.vdi' | grep -v 'Parent UUID' \
 | tr -s '\n' ' ' | sed 's/UUID/\nUUID/g' | awk '{print $1,$2,$3,$5,$6}' | grep '\.vdi' | awk '{print $1,$2,$3,$4,$5}' | sed 's/UUID:/ */g' \
